374d430688772f3563ebfd1d212786f05b05e3e3
SymbioticSnake <dananh.titongo@gmail.com>
2023-12-28 23:23:31 -0400

    Revert "Added Vector and Vector equation classes"
    This reverts commit 1fcb3e2ba5dd01bbadd58fb1844dec8f9b2ff409.

--- classes/ComplexNumber.cpp
+++ classes/ComplexNumber.cpp
@@ -1,67 +1,141 @@
-#include "ComplexNumber.h"
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
 
-void ComplexNumber::root(double n, ComplexNumber roots[MAX_ROOTS]) const {
-    for (int i = 0; i < MAX_ROOTS; i++)
-        roots[i] = ComplexNumber();
+const int MAX_ROOTS = 10;
 
-    for (int k = 0; k < n; k++) {
-        roots[k] = ComplexNumber(pow(modulus(), 1/n) * cos((argument() + 2*k*M_PI) / n),
-                                 pow(modulus(), 1/n) * sin((argument() + 2*k*M_PI) / n));
+class ComplexNumber {
+private:
+    double real, imaginary;
+
+public:
+    ComplexNumber() {
+        real = 0;
+        imaginary = 0;
     }
-}
 
-ComplexNumber ComplexNumber::operator+(ComplexNumber const &other) const {
-    ComplexNumber sum(real + other.real, imaginary + other.imaginary);
-    return sum;
-}
+    ComplexNumber(double Re, double Im) {
+        real = Re;
+        imaginary = Im;
+    }
 
-ComplexNumber ComplexNumber::operator-(ComplexNumber const &other) const {
-    ComplexNumber difference(real - other.real, imaginary - other.imaginary);
-    return difference;
-}
+    ComplexNumber(ComplexNumber const &other) {
+        real = other.real;
+        imaginary = other.imaginary;
+    }
 
-ComplexNumber ComplexNumber::operator*(double const &num) const {
-    ComplexNumber product(real * num, imaginary * num);
-    return product;
-}
+    double getReal() const {
+        return real;
+    }
 
-ComplexNumber ComplexNumber::operator*(ComplexNumber const &other) const {
-    ComplexNumber product(real * other.real - imaginary * other.imaginary,
-                          real * other.imaginary + other.real * imaginary);
-    return product;
-}
+    double getImaginary() const {
+        return imaginary;
+    }
 
-ComplexNumber ComplexNumber::operator/(double const &num) const {
-    ComplexNumber quotient(real / num, imaginary / num);
-    return quotient;
-}
+    double argument() const {
+        return atan2(imaginary, real);
+    }
 
-ComplexNumber ComplexNumber::operator/(ComplexNumber const &other) const {
-    ComplexNumber quotient = (*this) * other.conjugate() /
-                             (pow(other.real,2) + pow(other.imaginary, 2));
-    cout << quotient.real << " " << quotient.imaginary << endl;
-    return quotient;
-}
+    double modulus() const {
+        return sqrt(pow(real, 2) + pow(imaginary, 2));
+    }
 
-ostream& operator<<(ostream &Out, ComplexNumber const &num) {
-    if (num.real == 0 && num.imaginary != 0)
-        Out << "(" << num.imaginary << ") j";
-    else if (num.imaginary == 0 && num.real != 0)
-        Out << "(" << num.real << ")";
-    else
-        Out << "(" << num.real << ") + (" << num.imaginary << ") j";
-    return Out;
-}
+    ComplexNumber conjugate() const {
+        ComplexNumber conVal(real, -imaginary);
+        return conVal;
+    }
 
-istream& operator>>(istream &In, ComplexNumber &num) {
-    In >> num.real >> num.imaginary;
-    return In;
-}
+    bool operator==(ComplexNumber const &other) const {
+        return (real == other.real && imaginary == other.imaginary);
+    }
+
+    ComplexNumber power(double n) const {
+        ComplexNumber powNum(pow(modulus(), n)*cos(n * argument()),
+                             pow(modulus(), n)*sin(n * argument()));
+        return powNum;
+    }
+
+    void root(double n, ComplexNumber roots[MAX_ROOTS]) const {
+        for (int i = 0; i < MAX_ROOTS; i++)
+            roots[i] = ComplexNumber();
+
+        for (int k = 0; k < n; k++) {
+            roots[k] = ComplexNumber(pow(modulus(), 1/n) * cos((argument() + 2*k*M_PI) / n),
+                                     pow(modulus(), 1/n) * sin((argument() + 2*k*M_PI) / n));
+        }
+    }
+
+    ComplexNumber operator+(ComplexNumber const &other) const {
+        ComplexNumber sum(real + other.real, imaginary + other.imaginary);
+        return sum;
+    }
+
+    ComplexNumber operator-(ComplexNumber const &other) const {
+        ComplexNumber difference(real - other.real, imaginary - other.imaginary);
+        return difference;
+    }
+
+    ComplexNumber operator*(double const &num) const {
+        ComplexNumber product(real * num, imaginary * num);
+        return product;
+    }
+
+    ComplexNumber operator*(ComplexNumber const &other) const {
+        ComplexNumber product(real * other.real - imaginary * other.imaginary,
+                              real * other.imaginary + other.real * imaginary);
+        return product;
+    }
+
+    ComplexNumber operator/(double const &num) const {
+        ComplexNumber quotient(real / num, imaginary / num);
+        return quotient;
+    }
+
+    ComplexNumber operator/(ComplexNumber const &other) const {
+        ComplexNumber quotient = (*this) * other.conjugate() /
+                                 (pow(other.real,2) + pow(other.imaginary, 2));
+        cout << quotient.real << " " << quotient.imaginary << endl;
+        return quotient;
+    }
+
+    friend ostream& operator<<(ostream &Out, ComplexNumber const &num) {
+        if (num.real == 0 && num.imaginary != 0)
+            Out << "(" << num.imaginary << ") j";
+        else if (num.imaginary == 0 && num.real != 0)
+            Out << "(" << num.real << ")";
+        else
+            Out << "(" << num.real << ") + (" << num.imaginary << ") j";
+        return Out;
+    }
+
+    friend istream& operator>>(istream &In, ComplexNumber &num) {
+        In >> num.real >> num.imaginary;
+        return In;
+    }
+
+};
+
+ComplexNumber roots[MAX_ROOTS];
 
 ComplexNumber pow(ComplexNumber const &c, double n) {
     if (n == 0)
         return ComplexNumber(1, 0);
     else if (n > 0 && n < 1)
-        c.root(n, roots);
+         c.root(n, roots);
     else return c.power(n);
+}
+
+double abs(ComplexNumber const &c) {
+    return c.modulus();
+}
+
+double Re(ComplexNumber const &c) {
+    return c.getReal();
+}
+
+double Im(ComplexNumber const &c) {
+    return c.getImaginary();
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
-#include "classes/ComplexNumber.h"
-#include "classes/VectorMatrix.h"
+#include "classes/Matrix.cpp"
+#include "classes/ComplexNumber.cpp"
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() {
-    Vector v1;
-    v1.cin_vector();
-    cout << v1;
+    ComplexNumber c1(-16, 0);
+    c1.root(3, roots);
+
+    int power = 0;
+    cout << "Enter power to calculate root: ";
+    cin >> power;
+    c1.root(power, roots);
+
+    for (int i = 0; i < power; i++) {
+        cout << roots[i] << endl;
+    }
+    return EXIT_SUCCESS;
 }
